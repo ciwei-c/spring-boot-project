d0e59c01459f22af8251f0f08c537b464abb86e1
ciwei-c <ciwei_c@163.com>
2023-05-30 15:21:46 +0800

    feat:user 增删改查

--- src/main/java/com/c/controller/UserController.java
+++ src/main/java/com/c/controller/UserController.java
@@ -3,12 +3,19 @@ package com.c.controller;
 import com.c.config.BaseResult;
 import com.c.mapper.UserMapper;
 import com.c.model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
@@ -22,5 +29,33 @@ public class UserController {
     public BaseResult<List<User>> getAllUser(){
         List<User> users = userMapper.getAll();
         return BaseResult.successWithData(users);
+    }
+
+    @GetMapping("/user")
+    public BaseResult<User> getUser(@RequestParam("userId") String userId){
+        User user = userMapper.getOne(userId);
+        return BaseResult.successWithData(user);
+    }
+
+    @PostMapping("/user")
+    public BaseResult<User> addUser(@RequestBody User user){
+        String userId = UUID.randomUUID().toString().replace("-", "");
+        user.setUserId(userId);
+        userMapper.insert(user);
+        User addedUser = userMapper.getOne(userId);
+        return BaseResult.successWithData(addedUser);
+    }
+
+    @DeleteMapping("/user")
+    public BaseResult<String> deleteUser(@RequestParam("userId") String userId){
+        userMapper.delete(userId);
+        return BaseResult.success();
+    }
+
+    
+    @PutMapping("/user")
+    public BaseResult<String> updateUser(@RequestBody User user){
+        userMapper.update(user);
+        return BaseResult.success();
     }
 }
